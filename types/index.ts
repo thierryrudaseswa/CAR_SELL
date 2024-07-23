3e44b7397c97d6bcdc38fde94d96ee85eee6b62b
thierryrudaseswa <trudaseswa@gmail.com>
2024-07-23 23:35:16 +0200

    a web application managing car listings and sales for both dealerships and sellers

---
+++ types/index.ts
@@ -0,0 +1,13 @@
+"use Client"
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps{
+    title:string;
+    btnType?: "button" | "submit";
+    containerStyles?:string;
+    handleClick?:MouseEventHandler<HTMLButtonElement>;
+}
+export interface SearchManufacturerProps{
+    manufacturer : string;
+    setManufacturer : (manufacturer : string) => void;
+}
